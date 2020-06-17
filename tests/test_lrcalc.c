f3bdbf8179614f83fcb8cad078e13001b00c149c
= <brewcrew5498@outlook.com>
2020-06-16 19:39:25 -0500

    Remove Catch2.

--- tests/test_lrcalc.c
+++ tests/test_lrcalc.c
@@ -1,12 +1,7 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
-#define CATCH_CONFIG_MAIN
-
-#include <catch2/catch.hpp>
 #include <lrcalc/symfcn.h>
 
-
-// TODO(you): Remove this unnecessary test case.
-TEST_CASE("Sanity Check", "[addition]") {
-  REQUIRE(1 + 1 == 2);
+int main() {
+  // TODO: Find a nice C unit testing framework.
 }
